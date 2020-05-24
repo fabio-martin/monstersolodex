aee38a157c8a29dbbe12af8d78856e394983ef41
Fabio Martin <fabiomartin2006@gmail.com>
2020-05-24 16:56:00 +0100

    Updates

---
+++ precache-manifest.a25aecc77fdd6d0b64ab9376b4ff430b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac381de11b27d1f6db660e869a977463",
+    "url": "/monstersolodex/index.html"
+  },
+  {
+    "revision": "f8706b7024ca38e537a6",
+    "url": "/monstersolodex/static/css/main.16be8e6a.chunk.css"
+  },
+  {
+    "revision": "92df4dc94a27cc91e8b4",
+    "url": "/monstersolodex/static/js/2.49fad4c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersolodex/static/js/2.49fad4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8706b7024ca38e537a6",
+    "url": "/monstersolodex/static/js/main.6a961e04.chunk.js"
+  },
+  {
+    "revision": "fa1e74bac2f0e5e32a5d",
+    "url": "/monstersolodex/static/js/runtime-main.b9d667cd.js"
+  }
+]);
